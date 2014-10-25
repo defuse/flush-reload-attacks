bb3f3f0ffac95df635f1def3476693aaf06cca70
Taylor Hornby <havoc@defuse.ca>
2014-10-25 15:46:39 -0600

    Improve systeminfo.sh

--- experiments/systeminfo.sh
+++ experiments/systeminfo.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function header {
+    echo ""
+    echo "-------------------------"
+    echo "$1"
+    echo "-------------------------"
+    echo ""
+}
+
+header "Basic Information"
+
 echo -n "DATE:    "
 date
 echo -n "UPTIME:  "
@@ -7,8 +17,14 @@ uptime
 echo -n "KERNEL:  "
 uname -a
 
-echo "MEMORY:"
+header "Memory Usage"
 free
 
-echo "PROCESSOR:"
+header "Disk Usage"
+df
+
+header "Processor"
 cat /proc/cpuinfo
+
+header "Compiler"
+gcc -v -v --version 2>&1
