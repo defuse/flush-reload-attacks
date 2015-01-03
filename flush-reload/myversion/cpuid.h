cfa407a5558a2bce7dbd9ed0dc145b42158f692e
Taylor Hornby <havoc@defuse.ca>
2015-01-03 12:21:15 -0700

    Move CPUID stuff to its own file

---
+++ flush-reload/myversion/cpuid.h
@@ -0,0 +1,50 @@
+#ifndef CPUID_H
+#define CPUID_H
+
+#include <stdint.h>
+
+typedef struct CpuIdRegisters {
+    uint32_t eax, ebx, ecx, edx;
+} cpuid_regs_t;
+
+void cpuid(uint32_t eax, cpuid_regs_t *regs)
+{
+    asm __volatile__ (
+        " movl %4, %%eax \n"
+        " cpuid \n"
+        " movl %%eax, %0 \n"
+        " movl %%ebx, %1 \n"
+        " movl %%ecx, %2 \n"
+        " movl %%edx, %3 \n"
+        : "=rm" (regs->eax), "=rm" (regs->ebx), "=rm" (regs->ecx), "=rm" (regs->edx)
+        : "irm" (eax)
+        : "%eax", "%ebx", "%ecx", "%edx"
+    );
+}
+
+/*
+ * 17.13.1 Invariant TSC (Intel Software Developer's Manual Volume 3B)
+ *
+ * The time stamp counter in newer processors may support an enhancement,
+ * reffered to as invariant TSC. Processor's support for invariant TSC is
+ * indicated by CPUID.80000007H:EDX[8].
+ *
+ * The invariant TSC will run at a constant rate in all ACPI P-, C-. and
+ * T-states. This is the architectural behavior moving forward. On processors
+ * with invariant TSC support, the OS may use the TSC for wall clock timer
+ * services (instead of ACPI or HPET timers). TSC reads are much more efficient
+ * and do not incur the overhead associated with a ring transition or access to
+ * a platform resource.
+ */
+int cpuid_has_invariant_tsc()
+{
+    cpuid_regs_t regs;
+    cpuid(0x80000007, &regs);
+    if (regs.edx & 0x10) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+#endif
--- flush-reload/myversion/flushreload.c
+++ flush-reload/myversion/flushreload.c
@@ -1,62 +1,19 @@
 #include <stdio.h>
 
-#include <stdint.h>
-
 #include "flushreload.h"
+#include "cpuid.h"
 
-typedef struct CpuIdRegisters {
-    uint32_t eax, ebx, ecx, edx;
-} cpuid_regs_t;
-
-void cpuid(uint32_t eax, cpuid_regs_t *regs)
+void checkSystemConfiguration()
 {
-    asm __volatile__ (
-        " movl %4, %%eax \n"
-        " cpuid \n"
-        " movl %%eax, %0 \n"
-        " movl %%ebx, %1 \n"
-        " movl %%ecx, %2 \n"
-        " movl %%edx, %3 \n"
-        : "=rm" (regs->eax), "=rm" (regs->ebx), "=rm" (regs->ecx), "=rm" (regs->edx)
-        : "irm" (eax)
-        : "%eax", "%ebx", "%ecx", "%edx"
-    );
-}
-
-int has_invariant_tsc()
-{
-    cpuid_regs_t regs;
-    cpuid(0x80000007, &regs);
-    if (regs.edx & 0x10) {
-        return 1;
-    } else {
-        return 0;
+    if (!cpuid_has_invariant_tsc()) {
+        printf("WARNING: This processor does not have an invariant TSC.\n");
     }
-}
 
-void print_vendor_info()
-{
-    cpuid_regs_t regs;
-    regs.eax = 0xFFFFFFFF;
-    regs.ebx = 0xFFFFFFFF;
-    regs.ecx = 0xFFFFFFFF;
-    regs.edx = 0xFFFFFFFF;
-    cpuid(0x00, &regs);
-    uint32_t vendor[4] = {regs.ebx, regs.edx, regs.ecx, 0};
-    int i = 0;
-    for (i = 0; i < 4; i++) { printf("%x\n", vendor[i]); }
-    const char *vendor_str = (char *)vendor;
-    printf("Vendor: %s\n", vendor_str);
+    /* TODO: warn on multiple procs */
 }
 
 void startSpying(args_t *args)
 {
-    printf("Spying!\n");
-
-    if (has_invariant_tsc()) {
-        printf("Yes!\n");
-    }
-
     /* Find the virtual address the victim binary gets loaded at. */
 
     /* Map the victim binary into our address space. */
