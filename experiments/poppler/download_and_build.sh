a98cbedcdf6d0457b6943a942caf5d94c29b97a9
Taylor Hornby <havoc@defuse.ca>
2015-05-13 12:27:53 -0400

    Fix script to support versions, add debian probe list

--- experiments/poppler/download_and_build.sh
+++ experiments/poppler/download_and_build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+VERSION="0.32.0"
+EXT="tar.xz"
+#VERSION="0.18.4"
+#EXT="tar.gz"
+
 cd /tmp/
-wget http://poppler.freedesktop.org/poppler-0.32.0.tar.xz
-rm -rf poppler-0.32.0
-tar xvf poppler-0.32.0.tar.xz
+wget http://poppler.freedesktop.org/poppler-$VERSION.$EXT
+rm -rf poppler-$VERSION
+tar xvf poppler-$VERSION.$EXT
 rm -rf /tmp/experiment_root/
 mkdir /tmp/experiment_root/
-cd poppler-0.32.0
+cd poppler-$VERSION
 ./configure --prefix=/tmp/experiment_root
 make
 make install
