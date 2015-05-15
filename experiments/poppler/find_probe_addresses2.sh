0249ee1dfe297892fae0009fd6e16241a46a4801
Taylor Hornby <havoc@defuse.ca>
2015-05-15 11:03:19 -0600

    Add second probe finding script

---
+++ experiments/poppler/find_probe_addresses2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ruby FindProbeAddresses.rb \
+    -n 'gmallocn' \
+    -n 'PSOutputDev::writePSString(GooString*)' \
+    -n 'PSOutputDev::drawString(GfxState*, GooString*)' \
+    -n 'PSOutputDev::updateTextShift(GfxState*, double)' \
+    -b /tmp/experiment_root/lib/libpoppler.so.51
