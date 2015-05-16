2384e60b1cda1a62863ea2496200f074b5003315
Taylor Hornby <havoc@defuse.ca>
2015-05-15 21:25:27 -0400

    Add links run on defuse.ca

---
+++ experiments/links/runs/0015/input/LinksExperiment.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+if ARGV[0].nil?
+  puts "Must pass output directory on command line!"
+  exit(false)
+end
+
+URL_LIST_FILE = "url_sets/wiki-top-100-of-2013-HTTPS.txt"
+#URL_LIST_FILE = "url_sets/small-test.txt"
+SAMPLE_COUNT = 10
+OUTPUT_DIR = ARGV[0]
+RUN_BINARY = "binaries/links-debian"
+SPY_BINARY = RUN_BINARY
+PROBES_PATH = "binaries/links-debian-discovered.probes"
+VICTIM_RUNS = 1
+STRING_TRUNCATE_LENGTH = 1000
+SLEEP_KILL = 7
+
+require '../../flush-reload/myversion/ruby/RunExperiment.rb'
